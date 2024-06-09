25b3735c592eac6740d149a70e1daa242218ac4e
Raihan <raihannady@gmail.com>
2024-06-09 20:03:49 +0700

    update todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+// import Todoform from "./components/Todoform.jsx";
+import Todoform from "./components/TodoForm";
+import Todos from "./components/Todos";
+import TodoItem from "./components/Todos/TodoItem";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Finish Progate React Course",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Have lunch with Guru Domba",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Study React with Ninja Ken",
+      completed: false,
+    },
+  ]);
+
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={styles.container}>
+        <h1 style={styles.title}>My Todo List</h1>
+        <Todos todos={todos} toggleCompleted={toggleCompleted} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <Todoform />
+      <Todos todos={todos} /> */}
     </>
-  )
+  );
 }
 
-export default App
+const styles = {
+  container: {
+    textAlign: "center",
+    padding: "12px",
+  },
+  title: {
+    fontSize: "36px",
+  },
+};
+
+export default App;
---
+++ src/components/TodoForm/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Todoform = () => {
+  return (
+    <>
+      <h1>My Todo List</h1>
+    </>
+  );
+};
+
+export default Todoform;
